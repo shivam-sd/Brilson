434a6ed46b8b7909d782133bf5dc515aab84da0c
Shivam Maurya <sm5708251@gmail.com>
2025-12-20 19:13:41 +0530

    add page

--- server/app.js
+++ server/app.js
@@ -31,7 +31,6 @@ app.use(bodyParser.json());
 
 app.use(cors({
     origin:process.env.BASE_URL,
-    origin:process.env.BASE_URL1,
     credentials:true,
        methods: ["GET", "POST", "PUT", "DELETE"],
 }));
